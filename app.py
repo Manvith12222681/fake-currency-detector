e96cd5f4a6abd29c63db96f6c3e8e7f4de615707
Ramisetty Manvith <Manvith134@gmail.com>
2026-04-10 13:07:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-# Streamlit main application
-# ==========================================
-# MAIN APPLICATION
-# ==========================================
+st.sidebar.title("About")
+st.sidebar.info("This system detects fake currency using AI.")
 import streamlit as st
 from config import MODEL_PATH, IMAGE_SIZE
 from model_loader import load_trained_model
 from utils import preprocess_image, predict
-from ui import load_css, header, show_result
+from ui import load_css, header, show_result, info_section
 
 # Page config
-st.set_page_config(page_title="Currency Detection", layout="centered")
+st.set_page_config(page_title="Currency Detector", layout="centered")
 
-# Load custom CSS
+# Load UI
 load_css()
-
-# Header UI
 header()
+
+# Info section
+info_section()
 
 # Load model
 model = load_trained_model(MODEL_PATH)
 
-# File uploader
-uploaded_file = st.file_uploader("Upload Currency Image", type=["jpg", "jpeg", "png"])
+# Upload section
+st.markdown("### Upload Currency Image")
+uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
-# Main logic
 if uploaded_file:
-    image, display_img = preprocess_image(uploaded_file, IMAGE_SIZE)
+    with st.spinner("Analyzing image..."):
+        image, display_img = preprocess_image(uploaded_file, IMAGE_SIZE)
 
     st.image(display_img, caption="Uploaded Image", use_column_width=True)
 
