d64fb20bc5904bd530de005566d93d2f09170773
Ramisetty Manvith <Manvith134@gmail.com>
2026-04-14 20:56:40 +0530

    model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,12 @@
-# ==========================================
-# DUMMY MODEL (FOR DEPLOYMENT)
-# ==========================================
-import random
+# model_loader.py
 
-def load_trained_model(path: str):
-    return "dummy_model"
+from tensorflow.keras.models import load_model
+
+def load_trained_model(path):
+    try:
+        model = load_model(path, compile=False)
+        print(" Model loaded successfully")
+        return model
+    except Exception as e:
+        print(" Error loading model:", e)
+        return None
