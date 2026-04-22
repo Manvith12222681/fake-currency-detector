ce8824cb7c0a986f7aafd5a19028d5803ac5b78d
Ramisetty Manvith <Manvith134@gmail.com>
2026-04-22 13:05:02 +0530

    model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,12 +1,6 @@
 # model_loader.py
 
-from tensorflow.keras.models import load_model
-
 def load_trained_model(path):
-    try:
-        model = load_model(path, compile=False)
-        print(" Model loaded successfully")
-        return model
-    except Exception as e:
-        print(" Error loading model:", e)
-        return None
+    # Cloud version (no TensorFlow)
+    print("⚠ Using dummy model (Streamlit Cloud version)")
+    return None
